4867bc27a14b76ade6c7231a3051eac535127456
Nicole Mutia <mutianicole@gmail.com>
2018-11-07 22:32:33 -0600

    version submitted to professor

--- main.cpp
+++ main.cpp
@@ -6,26 +6,55 @@
 
 using namespace std;
 
-//double inputNumber, evenNumber, oddNumber;
-int inputNumber, remainder;
+// Function prototypes
+void mainMenu();
+void oddOReven(int);
 
+bool doesTheUserWantToContinue = true;
+// The main function
 int main()
 {
     // Greet User
-    cout << "Hi there! I can help you determine if a number is even or odd \n\n"<< endl;
-    // Ask user to enter a number
-    cout << "Enter the number you want to find out if it's odd or even: ";
-    cin >> inputNumber;
-    // Is the number entered by the user even or odd?
-        // Calculation to determine if the number is even or odd
-        remainder = inputNumber % 2;
-        if ((remainder == 1) || (remainder == -1))
+    cout << "Hi there! I can help you determine if a number is even or odd \n"<< endl;
+    mainMenu();
+    return 0;
+}
+void mainMenu()
+{
+    int mainMenuChoice;
+    do
+    {
+        cout << "\n\nPlease choose an option from the menu below: " << endl;
+        cout << "> Enter a number you want to find out if it's odd or even: \n\n";
+        cout << "> Press any key if you want to exit \n" << endl;
+        cout << "-> ";
+        cin >> mainMenuChoice; // Get the user's choice
+
+        // Check if mainMenuChoice is an integer
+        if (mainMenuChoice)
         {
-        cout << "You entered an odd number" << remainder << endl;
+            // Compute the integer if it's even or odd
+            oddOReven(mainMenuChoice);
         }
         else
         {
-        cout << "You entered an even number" << remainder << endl;
+            // User did not enter an integer, so prevent from looping again
+            doesTheUserWantToContinue = false;
         }
-    return 0;
+    }
+    while(doesTheUserWantToContinue);
+}
+void oddOReven(int i)
+{
+    // It gets the remainder
+    int remainder = i % 2;
+    // If the remainder is equal to 1 or -1, then it's an odd number. Through -1, all negative numbers can be determined if it's odd or even.
+    if ((remainder == 1) || (remainder == -1))
+    {
+        cout << "You entered an odd number" << endl;
+    }
+    else
+    {
+        cout << "You entered an even number" << endl;
+    }
 }
